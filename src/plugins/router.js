6e68d790c41b4b0bdcd47582d4f5f29953180b2c
unknown <qambaralievd@gmail.com>
2023-04-28 22:42:15 +0500

    itere build

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,8 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {path: '/', name :'home', component: () => import('@/views/Home.vue')},
   {path: '/about', name :'about', component: () => import('@/views/About.vue')},
-  {path: '/experience', name :'experience', component: () => import('@/views/Experience.vue')},
-  {path: '/helloWorld', name :'helloWorld', component: () => import('@/components/HelloWorld.vue')},
+  {path: '/experience', name :'experience', component: () => import('@/views/Experience.vue')}
 ]
 
 const router = createRouter({
